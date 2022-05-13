2ac850616d72d6a1dd442fc7cc73e7bd08fcef4e
Arthur - GaecKo <arthur.deneyer@student.uclouvain.be>
2022-05-13 12:08:47 +0200

    Make use of json files as its easier. Changed and cleaned whole code

--- function.py
+++ function.py
@@ -1,45 +1,54 @@
 from random import randint
+import json
+
+def access_json():
+    with open("assets/data.json", 'r') as f:
+        return json.load(f)
+
+def write_json(data):
+    with open("assets/data.json", 'w') as f:
+        json.dump(data, f, indent=4)
 
 def get_quote():
-	with open("librairies/quotes.txt", encoding="utf-8") as f:
+	with open("assets/quotes.txt", encoding="utf-8") as f:
 		return f.readlines()[randint(0, 29)]
 
-def add_kill_death_stats(kill, death, pseudo):
-    with open('librairies/temp.txt', 'r', encoding='utf-8') as f:
-        content = f.readlines()
-    content.append(f'Pseudo: {pseudo}\n')
-    content.append(f'Kills: {kill}\n')
-    content.append(f'Deaths: {death}\n\n')
-    with open('librairies/temp.txt', 'w', encoding='utf-8') as f:
-        f.writelines(content)
+def update_kill_death_stats(kill, death, pseudo):
+    data = access_json()
+    if pseudo in data:
+        data[pseudo][kill] += int(kill)
+        data[pseudo][death] += int(death)
+    elif pseudo not in data:
+        data[pseudo] = {
+            "kill" : kill,
+            "death": death,
+            "points": 0
+        }
+    write_json(data)
 
-def check_pseudo_for_stat(pseudo):
-    with open('librairies/temp.txt', 'r', encoding='utf-8') as f:
-        content = f.readlines()
-    for i in range(0, len(content), 3):
-        try:
-            if content[i].split()[1] == pseudo:
-                return True
-        except:
-            pass
+def check_pseudo_in_data(pseudo):
+    data = access_json()
+    if pseudo in data:
+        return True
     return False
 
-def change_kill_death_stats(kill, death, pseudo):
-    with open('librairies/temp.txt', 'r', encoding='utf-8') as f:
-        content = f.readlines()
-    for i in range(0, len(content), 3):
-        #try:
-        if content[i].split()[1] == pseudo:
-            content[i] = f"Pseudo: {pseudo}\n"
-            content[i+1] = f"Kills: {int(content[i+1].split()[1]) + int(kill)}\n" 
-            content[i+2] = f"Kills: {int(content[i+2].split()[1]) + int(death)}\n"
-            break
-        #except Exception as e:
-            #print(e)
-            #pass
-    with open('librairies/temp.txt', 'w', encoding='utf-8') as f:
-        f.writelines(content)
-
 def show_kill_death_stats(pseudo):
-    if check_pseudo_for_stat(pseudo):
-        pass
+    data = access_json()
+    if pseudo in data:
+        return f"Le joueurs **{pseudo}** a __{data[pseudo]['kill']} kills__ et  __{data[pseudo]['death']} morts__."
+    return f"Je n'ai aucune donnée sur {pseudo}. Utilisez $kdstat `kill` `death` `pseudo` pour en ajouter."
+
+template = {
+    "pseudo1" : {
+        "kill": 'x',
+        'death': 'x',
+        'points': "10"
+    },
+    "pseudo2": {
+        "kill": 10,
+        'death': 5
+
+    }
+}
+
+write_json(template)
--- main.py
+++ main.py
@@ -3,43 +3,42 @@ import os
 from function import *
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-    print(f'{client.user} has been logged in. Bot is running.')
+	print(f'{client.user} has been logged in. Bot is running.')
+
 
 @client.event
 async def on_message(msg):
-    if msg.author == client.user:
-	    return 
+	if msg.author == client.user:
+		return
 # ------- Vérificateur de fonctionnement ------- #
-    if msg.content.startswith('$test'):
-        await msg.channel.send("Test working!")
-
+	if msg.content.startswith('$test'):
+		await msg.channel.send("Test working!")
+	
 # ------- Envoyeur de citations ------- #
-    elif msg.content.startswith('$quotes'):
-        await msg.channel.send(str("Here is a little quote :) \n\n" + "`" + get_quote() + "`"))
+	elif msg.content.startswith('$quotes'):
+		await msg.channel.send(str("Here is a little quote :) \n\n" + "`" + get_quote() + "`"))
 
 # ------- Ajout de stats kill/mort ------- #
-    elif msg.content.startswith('$kdstat'):
-        # try
-        message = msg.content.split()
-        pseudo = message[1]  
-        kill = message[2]  
-        death = message[3] 
-        if not check_pseudo_for_stat(pseudo):
-            add_kill_death_stats(kill, death, pseudo)
-            await msg.channel.send(f"Stat added for {pseudo}.")
-        else:
-            change_kill_death_stats(kill, death, pseudo)
-            await msg.channel.send(f"Stat modified for {pseudo}.")
-        #except
-            #await msg.channel.send("Wrong use of the command: \n $kdstat `pseudo` `kills` `deaths`\n ")
-        
+	elif msg.content.startswith('$kdstat'):
+		try:
+			message = msg.content.split()
+			pseudo = message[1]  
+			kill = message[2]  
+			death = message[3] 
+			if check_pseudo_in_data(pseudo):
+				await msg.channel.send(f"Stat modified for {pseudo}")
+			else:
+				await msg.channel.send(f"Stat added for {pseudo}")
+			update_kill_death_stats(kill, death, pseudo)	
+		except:
+			await msg.channel.send("Wrong use of the command: \n $kdstat `pseudo` `kills` `deaths`\n ")
+		
 # ------- Vérificateur de langages ------- #
-    for i in ["salope", "pute", "connard", "salaud", "tg", "bot de merde", 'merde', 'enculé']:
-        if i.lower() in msg.content.lower():
-            await msg.channel.send(f"{i}, sérieusement {msg.author}? nan mais t'as quoi dans la tête? Ptite merde va")
-    
-	
+	# for i in ["salope", "pute", "connard", "salaud", "tg", "bot de merde", 'merde', 'enculé']:
+	# 	if i.lower() in msg.content.lower():
+	# 		await msg.channel.send(f"{i}, sérieusement {msg.author}? nan mais t'as quoi dans la tête? Ptite merde va")
 
 client.run(os.environ['TOKEN'])
