d0cffa6e7747c667b68885f53a390d4a343a0fbc
Arthur - GaecKo <arthur.deneyer@student.uclouvain.be>
2022-05-13 16:59:18 +0200

    added functions like mvp, stat, topboard, ...

--- function.py
+++ function.py
@@ -5,8 +5,6 @@ def access_json():
     with open("assets/data.json", 'r') as f:
         return json.load(f)
 
-
-
 def write_json(data):
     with open("assets/data.json", 'w') as f:
         json.dump(data, f, indent=4)
@@ -17,24 +15,67 @@ def get_quote():
 
 def top_board():
     data = access_json()
-    result = []
-    for pseudo, stats in data.items():
-        for key, value in stats.items():
-            if result == []:
-                result.append()
-            pass
+    kills = []
+    kill_death = []
+    death = []
+    points = []
+    for pseudo, values in data.items():
+        kills.append([pseudo, values["kill"]])
+        if values["death"] == 0:
+            kill_death.append([pseudo, values["kill"]])
+        else:
+            kill_death.append([pseudo, values["kill"] / values["death"]])
+        death.append([pseudo, values["death"]])
+        points.append([pseudo, get_points(pseudo)])
 
+    for i in range(len(kills)):
+        kills.sort(key=lambda x: x[1], reverse=True)
+        kill_death.sort(key=lambda x: x[1], reverse=True)
+        death.sort(key=lambda x: x[1], reverse=True)
+        points.sort(key=lambda x: x[1], reverse=True)
+
+    top_board_text = "**__BEST K/D__ :bar_chart: ** :\n"
+    for i in range(len(kill_death)):
+        top_board_text += f"\t | {i+1}) *{kill_death[i][0]}*: {kill_death[i][1]} of k/d ratio \n"
+
+    top_board_text += "\n\n**__MOST MVP POINTS__ :video_game: ** :\n"
+    for i in range(len(points)):
+        top_board_text += f"\t | {i+1}) *{points[i][0]}*: {points[i][1]} mvp points \n"
+    
+    top_board_text += "\n\n**__MOST KILLS__ :chart_with_upwards_trend: ** :\n"
+    for i in range(len(kills)):
+        top_board_text += f"\t | {i+1}) *{kills[i][0]}*: {kills[i][1]} kills \n"
+    
+    top_board_text += "\n\n**__MOST DEATHS__ :skull_crossbones: ** :\n"
+    for i in range(len(death)):
+        top_board_text += f"\t | {i+1}) *{death[i][0]}*: {death[i][1]} death \n"
+    
+    return top_board_text
+
+def update_mvp(pseudo, pos):
+    data = access_json()
+    data[pseudo][pos] += 1
+    write_json(data)
+
+def get_points(pseudo):
+    data = access_json()
+    player_info = data[pseudo]
+    points = 0
+    points += player_info["1"] + (player_info["2"] / 2) + (player_info["3"] / 3)
+    return points 
 
 def update_kill_death_stats(kill, death, pseudo):
     data = access_json()
     if pseudo in data:
-        data[pseudo]["kill"] += int(kill)
-        data[pseudo]["death"] += int(death)
+        data[pseudo]["kill"] += kill
+        data[pseudo]["death"] += death
     elif pseudo not in data:
         data[pseudo] = {
             "kill" : int(kill),
             "death": int(death),
-            "points": 0
+            "1": 0,
+            "2": 0,
+            "3": 0
         }
     write_json(data)
 
@@ -44,18 +85,31 @@ def check_pseudo_in_data(pseudo):
         return True
     return False
 
-def show_kill_death_stats(pseudo):
+def show_stats(pseudo):
     data = access_json()
     if pseudo in data:
-        return f"Le joueurs **{pseudo}** a __{data[pseudo]['kill']} kills__ et  __{data[pseudo]['death']} morts__."
+        if data[pseudo]["death"] != 0:
+            k_d = data[pseudo]["kill"] / data[pseudo]["death"]
+        else:
+            k_d = data[pseudo]["kill"]
+        return f"Le joueurs **{pseudo}** a __{data[pseudo]['kill']} kills__ et  __{data[pseudo]['death']} morts__. Son k/d est de {k_d} et ses points mvp sont {get_points(pseudo)}"
     return f"Je n'ai aucune donnée sur {pseudo}. Utilisez $kdstat `kill` `death` `pseudo` pour en ajouter."
 
+def del_user(pseudo):
+    data = access_json()
+    del data[pseudo]
+    write_json(data)
+    
 def help():
     return """
     Here are somme command you can use:
     $help -> send you help
     $quotes -> send a gaming quote
-    $kdstat `pseudo` `kills` `deaths`-> Update K/D of a player
+    $kdstat `pseudo` `kills` `deaths`-> Update K/D of a player, kills and deaths must be numbers
+    $stat `pseudo` -> shows the stat of a player 
+    $delete `pseudo` -> deletes all data about a player
+    $mvpstat `pseudo` `position` -> position must be 1 or 2 or 3. add position info of a player
+    $topboard -> Shows the general top board with the known stats
     $git -> send the git repository of this bot
     """
 
--- main.py
+++ main.py
@@ -19,19 +19,40 @@ async def on_message(msg):
 # ------- Vérificateur de fonctionnement ------- #
 	elif msg.content.startswith('$test'):
 		await msg.channel.send("Test working!")
-	
+
+# ------- Ajout de stats MVP ------- #
+	elif msg.content.startswith("$mvpstat"):
+		try:
+			message = msg.content.split()
+			pseudo = message[1]
+			pos = message[2]
+			if pos not in ["1", "2", "3"]:
+				await msg.channel.send("Parameters `pos` must be '1' or '2' or '3'")
+			if check_pseudo_in_data(pseudo):
+				update_mvp(pseudo, pos)
+				await msg.channel.send(f"mvp stat added for {pseudo}.\n ")
+			else:
+				update_kill_death_stats(0, 0, pseudo)
+				update_mvp(pseudo, pos)
+				await msg.channel.send(f"mvp stat added for {pseudo}. Had to create a new player\n ")
+		except:
+			await msg.channel.send("Wrong use of the command: \n $mvpstat `pseudo` `pos`\n ")
 # ------- Help ------- #
 	elif msg.content.startswith("$help"):
 		await msg.channel.send(help())
 
 # ------- Stats ------- #
 	elif msg.content.startswith("$stat"):
-		try:
-			message = msg.content.split()
-			pseudo = message[1]
-			await msg.channel.send(show_kill_death_stats(pseudo))
-		except:
-			await msg.channel.send("Wrong use of the command: \n $stat `pseudo`\n ")
+		# try:
+		message = msg.content.split()
+		pseudo = message[1]
+		await msg.channel.send(show_stats(pseudo))
+		# except:
+		# 	await msg.channel.send("Wrong use of the command: \n $stat `pseudo`\n ")
+
+# ------- LEADERBOARD ------- #
+	elif msg.content.startswith("$topboard") or msg.content.startswith("$top"):
+		await msg.channel.send(top_board())
 
 # ------- Git ------- #
 	elif msg.content.startswith("$git"):
@@ -46,20 +67,33 @@ async def on_message(msg):
 		try:
 			message = msg.content.split()
 			pseudo = message[1]  
-			kill = message[2]  
-			death = message[3] 
+			kill = int(message[2])  
+			death = int(message[3]) 
 			if check_pseudo_in_data(pseudo):
 				await msg.channel.send(f"Stat modified for {pseudo}")
 			else:
 				await msg.channel.send(f"Stat added for {pseudo}")
 			update_kill_death_stats(kill, death, pseudo)	
 		except:
-			await msg.channel.send("Wrong use of the command: \n $kdstat `pseudo` `kills` `deaths`\n ")
+			await msg.channel.send("Wrong use of the command: \n $kdstat `pseudo` `kills` `deaths`, kills and deaths must be numbers\n ")
+	
+# ------- Delete pseudo ------- #
+	elif msg.content.startswith('$delete'):
+		try:
+			message = msg.content.split()
+			pseudo = message[1]
+			if check_pseudo_in_data(pseudo):
+				del_user(pseudo)
+				await msg.channel.send(f"{pseudo} a été supprimé.")
+			else:
+				await msg.channel.send(f"Je n'ai aucune information sur {pseudo}, je ne peux donc rien supprimer.")
+		except:
+			await msg.channel.send("Wrong use of the command: \n $delete `pseudo`\n ")
+	
+# ------- Command not found ------- #
+	elif msg.content.startswith('$'):
+		await msg.channel.send(f"Command {msg.content} not known, type `$help` if needed.")
 		
-# ------- Vérificateur de langages ------- #
-	# for i in ["salope", "pute", "connard", "salaud", "tg", "bot de merde", 'merde', 'enculé']:
-	# 	if i.lower() in msg.content.lower():
-	# 		await msg.channel.send(f"{i}, sérieusement {msg.author}? nan mais t'as quoi dans la tête? Ptite merde va")
 
 
 client.run(os.getenv('TOKEN')) 
